50512bce2d50fc897436c6ea9b89271a7d78d3a7
achicyr <hi.cyril@gmail.com>
2022-06-13 08:47:26 +0900

    WIP ajout des solution/screencast/exercices in branch main

---
+++ P3C3/screencast/src/pages/Freelances/index.test.js
@@ -0,0 +1,47 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import {
+  waitFor,
+  waitForElementToBeRemoved,
+  screen,
+} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { render } from '../../utils/test'
+import { ThemeProvider } from '../../utils/context'
+import Freelances from './'
+
+const freelancersMockedData = [
+  {
+    name: 'Harry Potter',
+    job: 'Magicien frontend',
+    picture: '',
+  },
+  {
+    name: 'Hermione Granger',
+    job: 'Magicienne fullstack',
+    picture: '',
+  },
+]
+
+const server = setupServer(
+  rest.get('http://localhost:8000/freelances', (req, res, ctx) => {
+    return res(ctx.json({ freelancersList: freelancersMockedData }))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+it('Should display freelancers names', async () => {
+  render(
+    <ThemeProvider>
+      <Freelances />
+    </ThemeProvider>
+  )
+
+  await waitForElementToBeRemoved(() => screen.getByTestId('loader'))
+
+  expect(screen.getByText('Harry Potter')).toBeInTheDocument()
+  expect(screen.getByText('Hermione Granger')).toBeInTheDocument()
+})
